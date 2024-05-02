c32d99c3aeb3a65eec0840b84760d85e676dd4f9
anais <mouelhi.chacha@gmail.com>
2024-05-02 16:20:03 +0200

    Your commit message

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     googleClientId: '154063165170-j4d87oamru8tm9tlr6t85ehjm1mn6vbc.apps.googleusercontent.com',
-    apiUrl: 'https://back-end-angular-app.netlify.app',
+    apiUrl: 'https://main--back-end-angular-app.netlify.app/',
     open_weather_api_key: "07e6c440f864f9dbfd33a5035c3b536c",
 };
