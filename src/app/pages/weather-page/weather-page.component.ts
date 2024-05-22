4ed0442d790a2838a2827e6e681a641e39cfdc4b
anais <mouelhi.chacha@gmail.com>
2024-05-22 16:20:02 +0200

    Amélioration du code HTML

--- src/app/pages/weather-page/weather-page.component.ts
+++ src/app/pages/weather-page/weather-page.component.ts
@@ -9,6 +9,7 @@ import { AirQuality, WxModel } from '../../../models/weather.model';
 import { LeafletMapComponent } from '../../components/leaflet-map/leaflet-map.component';
 import { WxDisplayComponent } from '../../components/wx-display/wx-display.component';
 import { ToastrService } from 'ngx-toastr';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 
 @Component({
@@ -20,7 +21,9 @@ import { ToastrService } from 'ngx-toastr';
     RouterLink,
     TitleCasePipe,
     LeafletMapComponent,
-    WxDisplayComponent
+    WxDisplayComponent,
+    SlickCarouselModule,
+    DatePipe,
   ],
   templateUrl: './weather-page.component.html',
   styleUrl: './weather-page.component.css'
@@ -37,6 +40,7 @@ export class WeatherPageComponent implements OnInit{
   public airQuality: string = '';
   public todayLocale: string = '';
   public today: Date = new Date();
+  public slideConfig!: any;
 
 
   constructor(
@@ -47,13 +51,13 @@ export class WeatherPageComponent implements OnInit{
   {
     this.activePage = "meteo";
     this.todayLocale = this.getCurrentDay();
+    this.slideConfig = {"slidesToShow": 3, "slidesToScroll": 2};
     // verification de l'authentification
     this.authService.profile$.subscribe((profile) => {
       if (profile) {
         this.loading = false;
         this.profile = profile;
         this.profile.token = this.authService.getIdToken();
-
         
       }
     });
@@ -107,6 +111,27 @@ export class WeatherPageComponent implements OnInit{
   getCurrentDay(): string {
     const today = new Date();
     return today.toLocaleDateString('fr-FR');
+  }
+  async loadWxHistory() {
+    const wx = await this.weatherService.getUserHistory(this.profile.email, this.profile.token);
+    // console.log(wx);
+    this.weatherData = wx;
+    this.loadingWx = false;
+  }
+
+  slickInit(event: any) {
+    console.log('slick initialized', event);
+  }
+
+  async removeHistory() {
+    console.log("kjhkjhkj");
+    this.loadingWx = true;
+    this.loading = true;
+    const res = await this.weatherService.deleteUserHistory(this.profile.email, this.profile.token)
+    await this.loadWxHistory()
+    console.log(res)
+    this.loadingWx = false;
+    this.loading = false;
   }
 
   async logout(): Promise<void> {
