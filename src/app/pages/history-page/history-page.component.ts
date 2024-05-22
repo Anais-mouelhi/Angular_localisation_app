b352614349cf458896c35f0c3facd467484e654b
anais <mouelhi.chacha@gmail.com>
2024-05-22 19:42:42 +0200

    Amélioration du code HTML

--- src/app/pages/history-page/history-page.component.ts
+++ src/app/pages/history-page/history-page.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { AuthGoogleService } from '../../services/auth.service';
 import { WxService } from '../../services/weather.service';
 import { User } from '../../../models/user.model';
-import { CommonModule, DatePipe } from '@angular/common';
+import { CommonModule, DatePipe, TitleCasePipe  } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { RouterLink } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { LeafletMapComponent } from '../../components/leaflet-map/leaflet-map.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
-import { WxModel } from '../../../models/weather.model';
+import { WxModel, AirQuality } from '../../../models/weather.model';
+
+import { WxDisplayComponent } from '../../components/wx-display/wx-display.component';
+
 
 @Component({
   selector: 'app-history-page',
@@ -20,6 +23,8 @@ import { WxModel } from '../../../models/weather.model';
     LeafletMapComponent,
     SlickCarouselModule,
     DatePipe,
+    TitleCasePipe,
+    WxDisplayComponent
   ],
   templateUrl: './history-page.component.html',
   styleUrl: './history-page.component.css'
@@ -31,6 +36,11 @@ export class HistoryPageComponent implements OnInit{
   public loadingWx: boolean = true;
   public weatherData!: any;
   public slideConfig!: any;
+  public airQualityData!: any;
+  public cityName: string = '';
+  public airQuality: string = '';
+  public todayLocale: string = '';
+  public today: Date = new Date();
 
   constructor(
     private authService: AuthGoogleService,
@@ -63,6 +73,40 @@ export class HistoryPageComponent implements OnInit{
     .catch((e) => {
       console.log(e);
     })
+  }
+
+  private async loadWeatherData() {
+    const coords = await this.weatherService.getCurrentCoordinates();
+    // console.log(coords);
+
+    const location = await this.weatherService.getCityName(coords);
+    // console.log(location);
+
+    const cityName = location.address.city || location.address.town || location.address.village;
+
+    const weatherData: any = await this.weatherService.getWx(cityName);
+    // console.log(weatherData);
+
+    const airQuality = await this.weatherService.getAirQuality(coords);
+    // console.log(airQuality);
+
+    const weather: WxModel = {
+      email: this.profile.email,
+      lat: coords.lat,
+      lon: coords.lon,
+      city: cityName,
+      weather: weatherData,
+      airQuality: (airQuality as AirQuality).list?.[0]?.main?.aqi,
+    };
+    this.weatherData = weather;
+    // console.log(this.weatherData);
+    this.loadingWx = false;
+    await this.weatherService.storeWx(weather, this.profile.token);
+  }
+
+  getCurrentDay(): string {
+    const today = new Date();
+    return today.toLocaleDateString('fr-FR');
   }
 
   async loadWxHistory() {
